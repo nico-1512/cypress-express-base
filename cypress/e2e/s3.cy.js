846beac627824678d2d45a407f2b115ea6873f8d
Nicolas D'Ambrosio <dambrosio.nico02@gmail.com>
2023-10-24 13:00:16 +0200

    feat: edit s3.cy Dev-by: @nico-1512

--- cypress/e2e/s3.cy.js
+++ cypress/e2e/s3.cy.js
@@ -3,6 +3,14 @@ describe('s3 testing', () => {
 
   let ETag;
 
+  before(() => {
+    cy.request('GET', '/s3').then(
+      (response) => {
+        expect(response.status).to.eq(200);
+      }
+    )
+  })
+
   it('insert into bucket', () => {
     cy.request('POST', '/s3', {filePath: '/home/nico/Desktop/blank.pdf'}).then(
       (response) => {
